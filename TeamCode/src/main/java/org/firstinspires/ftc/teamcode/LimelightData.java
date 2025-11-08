5db80a94d51b5bf722e581960b6d942ccdbe9b79
AidenLu75 <114450389+AidenLu75@users.noreply.github.com>
2025-11-07 20:00:56 -0500

    Update LimelightData.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightData.java
@@ -1,37 +1,41 @@
 package org.firstinspires.ftc.teamcode;
-// Imports for limelight, OpMode for teleop, etc.
+
 import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-// Code/file used for AprilTag Detection and Measuring distance between bot and tag
-
+@TeleOp(name = "Limelight Data", group = "Test")
 public class LimelightData extends OpMode {
 
     private Limelight3A limelight;
     private IMU imu;
     private double distance;
-    // private DcMotorEx launcher = null;
-
 
     @Override
     public void init() {
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(8); // April Tag pipeline
-        imu = hardwareMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
-        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+        telemetry.addLine("Initializing...");
+        telemetry.update();
 
+        // Initialize Limelight and IMU
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(8); // Switch to AprilTag pipeline
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+        );
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+        telemetry.addLine("Initialization complete");
+        telemetry.update();
     }
 
     @Override
@@ -43,25 +47,22 @@ public class LimelightData extends OpMode {
     public void loop() {
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
         limelight.updateRobotOrientation(orientation.getYaw());
+
         LLResult llResult = limelight.getLatestResult();
+
         if (llResult != null && llResult.isValid()) {
             Pose3D botPose = llResult.getBotpose();
-            // Data with Control Hub presented
-            distance = getDistance(llResult.getTa());
-            telemetry.addData("Tx", llResult.getTx()); // Horizontal Offset (left/right)
-            telemetry.addData("Ty", llResult.getTy()); // Vertical Offset (up/down)
-            telemetry.addData("Ta", llResult.getTa()); // Target Area (sort of center)
-            telemetry.addData("botPose", botPose.toString()); // Position of robot
-            telemetry.addData("Distance from Tag", distance); // Distance from robot to tag
+
+            telemetry.addData("Target Valid", true);
+            telemetry.addData("Tx (Horizontal Offset)", llResult.getTx());
+            telemetry.addData("Ty (Vertical Offset)", llResult.getTy());
+            telemetry.addData("Ta (Target Area)", llResult.getTa());
+            telemetry.addData("Bot Pose", botPose.toString());
+        } else {
+            telemetry.addData("Target Valid", false);
+            telemetry.addLine("No valid AprilTag detected.");
         }
-    }
 
-
-    // Code for measuring distance
-    public double getDistance(double ta) {
-        double scale = 4000.0; // Test (Still need to get data)
-        distance = (scale / ta);
-        return distance;
+        telemetry.update();
     }
 }
-
