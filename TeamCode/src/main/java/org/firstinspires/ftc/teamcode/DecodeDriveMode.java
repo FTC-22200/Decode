9d939dec50685da20ddb391194337f17429e4703
rohan-va <rohan.y.vaidya@gmail.com>
2025-11-23 12:59:07 -0500

    box + new stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeDriveMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeDriveMode.java
@@ -11,29 +11,30 @@ import com.qualcomm.robotcore.util.SerialNumber;
 
 @TeleOp
 public class DecodeDriveMode extends LinearOpMode {
-    private DcMotorEx frontLeft = null;
-    private DcMotorEx backLeft = null;
-    private DcMotorEx frontRight = null;
-    private DcMotorEx backRight = null;
+    double launcher_power = 1.0;
+    double launcher_velocity = 3000.0;
+    private DcMotor intakeMotor;
+    private DcMotorEx launcher;
     private CRServo boxServo = null;
+
     @Override
     public void runOpMode() {
 
         // Motor config
-        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
-        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
-        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
-        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
-        boxServo = hardwareMap.get(CRServo.class, "boxServo");
-        DcMotor intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
+        DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class,"backLeft");
+        DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class,"frontRight");
+        DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
+        DcMotorEx launcher = hardwareMap.get(DcMotorEx.class, "launcherMotor");
 
-        // Servo config
-
+        // Motor directions
         frontLeft.setDirection(DcMotorEx.Direction.FORWARD);
         backLeft.setDirection(DcMotorEx.Direction.FORWARD);
         frontRight.setDirection(DcMotorEx.Direction.REVERSE);
         backRight.setDirection(DcMotorEx.Direction.REVERSE);
         intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        launcher.setDirection(DcMotorEx.Direction.FORWARD);
 
         final int CYCLE_MS = 50;
 
@@ -41,53 +42,87 @@ public class DecodeDriveMode extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+            if (gamepad2.y) {
+                launch();
+            } else {
+                launcher.setPower(0.0);
+            }
 
             double speedMultiplier = 1.0;
-
-
             if (gamepad1.left_trigger > 0.5) {
-                speedMultiplier = 0.25; // Reduce speed by a quarter when you hold left trigger
+                speedMultiplier *= 0.4; // Original - prev was 0.5
             }
-            double y = -gamepad1.left_stick_y * speedMultiplier; // For forwards/backwards movement
-            double x = gamepad1.left_stick_x * 1.1 * speedMultiplier; // The 1.1 multiplier is to counteract imperfect strafing
-            double rx = -gamepad1.right_stick_x * speedMultiplier; // Turning left/right
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1); // Ensures motor values stay within [-1, 1]
-            double fL_Motor = (y + x + rx) / denominator; // fL = FrontLeft
-            double bL_Motor = (y - x + rx) / denominator; // bL = BackLeft
-            double fR_Motor = (y - x - rx) / denominator; // fR = FrontRight
-            double bR_Motor = (y + x - rx) / denominator; // bR = BackRight
 
-            frontLeft.setVelocity(fL_Motor*2000.0);
-            backLeft.setVelocity(bL_Motor*2000.0);
-            frontRight.setVelocity(fR_Motor*2000.0);
-            backRight.setVelocity(bR_Motor*2000.0);
+            if (gamepad1.left_bumper) {
+                frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+                frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+                backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+                backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            } else {
+                frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+                frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+                backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+                backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+            }
+
+            // Drive calculations
+            double y = -gamepad1.left_stick_y * speedMultiplier;
+            double x = gamepad1.left_stick_x * 1.1 * speedMultiplier;
+            double rx = -gamepad1.right_stick_x * speedMultiplier;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+            // To decrease 'noise' via small movements
+            if (Math.abs(x) < 0.05) x = 0;
+            if (Math.abs(y) < 0.05) y= 0;
+            if (Math.abs(rx) < 0.05) rx = 0;
+
+            double fL_Motor = (y + x + rx) / denominator;
+            double bL_Motor = (y - x + rx) / denominator;
+            double fR_Motor = (y - x - rx) / denominator;
+            double bR_Motor = (y + x - rx) / denominator;
+
             frontLeft.setPower(fL_Motor);
             backLeft.setPower(bL_Motor);
             frontRight.setPower(fR_Motor);
             backRight.setPower(bR_Motor);
 
-            // Linear motor control
-
-            // Run intakeMotor
-            if (gamepad2.right_stick_y < 0.0) {
-                intakeMotor.setPower(gamepad2.right_stick_y);
-            } else if (gamepad2.right_stick_y > 0.0) {
+            // Intake motor control
+            if (gamepad2.right_stick_y != 0.0) {
                 intakeMotor.setPower(gamepad2.right_stick_y);
             } else {
                 intakeMotor.setPower(0.0);
             }
 
-            // Box servo to push the ball into the box
+            if (gamepad2.left_stick_y > 0.0) {
+                launcher_velocity += 100;
+            } else if (gamepad2.left_stick_y < 0.0) {
+                launcher_velocity -= 100;
+            }
+
+            // Box servo - push ball to box
             if (gamepad2.a) {
-                boxServo.setPower(0.6);
+                boxServo.setPower(-0.4);
             } else {
                 boxServo.setPower(0.0);
             }
-                telemetry.update();
-                sleep(CYCLE_MS);
-                idle();
-            }
+
+            telemetry.update();
+            telemetry.addData("Launcher Velocity : ", launcher_velocity);
+            sleep(CYCLE_MS);
+            idle();
         }
     }
 
-
+    private void launch() {
+        if (gamepad2.dpad_up) { // high
+            launcher_velocity = 2700.0;
+        } else if (gamepad2.dpad_left) { // medium
+            launcher_velocity = 2400.0;
+        } else if (gamepad2.dpad_right) { // low-mid (new)
+            launcher_velocity = 1800.0;
+        } else if (gamepad2.dpad_down) { // low
+            launcher_velocity = 1200.0 ;
+        }
+        launcher.setVelocity(launcher_velocity);
+    }
+}
